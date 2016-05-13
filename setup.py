0886e627f0b6a96d70e16d037fc15c5481a38a7a
Anup Pokhrel <virtualanup@gmail.com>
2016-05-13 13:19:27 +0545

    Added license and setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='nepalicalendar',
+    packages=['nepalicalendar'],
+    version='0.1',
+    description='Nepali Calendar Library',
+    author='Anup Pokhrel',
+    author_email='virtualanup@gmail.com',
+    url='https://github.com/nepalicalendar/nepalicalendar-py',
+    download_url='https://github.com/nepalicalendar/nepalicalendar-py/tarball/0.1',
+    keywords=['calendar', 'nepalicalendar', 'nepcal'],
+    classifiers=[],
+)
